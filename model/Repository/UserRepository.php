fa032b8244374c07e00b6f4e49d085600a4c099a
bentabol <117272448+bentabol@users.noreply.github.com>
2025-04-22 10:19:10 +0200

    Add files via upload

---
+++ model/Repository/UserRepository.php
@@ -0,0 +1,192 @@
+<?php
+namespace Model\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Model\Entity\bentatecnologies_usuarios;
+
+class UserRepository extends EntityRepository
+{
+   
+                                
+        /*trabajo*/ 
+        public function newUser ($c_username,$c_email,$c_nombre,$c_apellidos,$c_password,$n_idtipo_usuario) {
+            //Si fuera añadir un user nuevo ----> $user = new bentastore_usuarios() y ya despues seria igual
+            $user = new bentatecnologies_usuarios($c_username,$c_email,$c_nombre,$c_apellidos,$c_password,$n_idtipo_usuario);
+            if($user) {
+                //Hacerlo con todos los argumentos de entrada
+                $user->setc_nickname($c_username);
+                $user->setc_email($c_email);
+                $user->setc_nombre($c_nombre);
+                $user->setc_apellidos($c_apellidos);
+                $user->setc_password($c_password);
+                $user->setn_idtipo_usuario($n_idtipo_usuario);
+                $user->setI_activo(true);
+                $user->sett_conexiones(true);
+                //Insertaria el usuario
+                $this->_em->persist($user);
+                $this->_em->flush();
+            }
+
+        }
+        
+        public function updateUser($c_username, $c_email, $c_nombre, $c_apellidos, $c_password) {
+        $user = $this->findOneBy(['c_nickname' => $c_username]);
+
+        if ($user) {
+            // Set all the properties that need to be updated
+            $user->setc_email($c_email);
+            $user->setc_nombre($c_nombre);
+            $user->setc_apellidos($c_apellidos);
+            $user->setc_password($c_password);
+
+            // Persist and flush the changes
+            $this->_em->persist($user);
+            $this->_em->flush();
+
+            return true;
+        }
+
+        return false;
+    }
+
+        public function deleteUser ($c_username,$c_email,$c_nombre,$c_apellidos,$c_password) {
+            $user = $this->findOneBy(['c_nickname'=>$c_username]);
+            if($user) {
+            $this->_em->persist($user);
+                $this->_em->remove($user);
+            }
+
+        }
+        
+        public function loginUser ($t_conexiones, $n_idusero) {
+            $logeado = new bentatecnologies_usuarios($t_conexiones,$n_idusero);
+            if($logeado) {
+                //Hacerlo con todos los argumentos de entrada
+                $logeado->sett_conexiones($t_conexiones);
+                $logeado->setid_usuario($n_idusero);
+                //Insertaria el usuario
+                $this->_em->persist($logeado);
+                $this->_em->flush();
+            }
+
+        }
+       //Verifica q la sesion de ese usuario ya existe y no puedes registrarte ya que son exactamente los mismos datos
+        public function usuarioExiste($nickname, $contrasena)
+        {                                            
+            $sesionExiste = $this->findOneBy(['c_nickname'=>$nickname, 'c_password'=>$contrasena]);
+            if($sesionExiste){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        
+
+        //Indica que ya hay un usuario con ese nombre
+        public function usernameExists($nickname, $email)
+        {
+            $userExists = $this->findOneBy(['c_nickname'=>$nickname, 'c_email'=>$email]);
+                if($userExists){
+                    return true;
+                } else {
+                    return false;
+                }
+        }
+
+        public function esActivo($idUsuario)
+        {
+            $userActive = $this->findOneBy(['id_usuario'=>$idUsuario]);
+            if ($userActive->geti_activo() == 1) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
+        
+        public function usuarioActivo($activo)
+        {
+            if ($activo == 1) {
+                return "Activo";
+            } else {
+                return "Inactivo";
+            }
+        }
+
+        public function tipoUsuario($tipoUser)
+        {
+            $tipoUserId = $tipoUser->getid_tipo_usuario();
+            if ($tipoUserId == 1) {
+                return "Administrador";
+            } else if ($tipoUserId == 2) {
+                return "Gestor";
+            } else if($tipoUserId == 3) {
+                return "Cliente";
+            }
+        }
+        
+        public function comprobarNameEmail($nombreusuario,$correo)
+        {
+            $recuperarPassword = $this->findOneBy(['c_nickname'=>$nombreusuario, 'c_email'=>$correo]);
+            if($recuperarPassword){
+                return $recuperarPassword->getc_password();
+            } else {
+                return false;
+            }
+        }
+    
+        public function getDatosUsuario($nombreUser)
+        {
+            $user= $this->findOneBy(['c_nickname'=>$nombreUser]);
+                if($user){
+                    return $user;
+                } else {
+                    return null;
+                }
+        }
+
+    
+        public function getUsersOrderActives($filtroRol = '', $searchNickname = '')
+        {
+            $qb = $this->_em->createQueryBuilder();
+            $qb->select('u')->from('\Model\Entity\bentatecnologies_usuarios', 'u')->innerJoin('u.n_idtipo_usuario', 'tu')->orderBy('u.c_nickname', 'ASC');
+
+            if (!empty($filtroRol)) {
+                $qb->where('tu.c_descripcion = :filtroRol')->setParameter('filtroRol', $filtroRol);
+            }
+
+            if (!empty($searchNickname)) {
+                $qb->andWhere('u.c_nickname LIKE :searchNickname')->setParameter('searchNickname', '%'. $searchNickname. '%');
+            }
+          
+            $query = $qb->getQuery();
+            $actives = $query->getResult();
+
+            return $actives;
+        }
+    
+
+
+        public function getCreadorProyect($userID)
+        {
+            $creadorProyect= $this->findOneBy(['id_usuario'=>$userID]);
+            if($creadorProyect){
+                return $creadorProyect;
+            } else {
+                return null;
+            }
+        }
+
+        public function getUserById($idUsuario)
+        {
+            $nicknameID= $this->findOneBy(['id_usuario'=>$idUsuario]);
+            if($nicknameID){
+                return $nicknameID;
+            } else {
+                return null;
+            }
+        }
+
+
+    
+}
---
+++ partials/header.php
@@ -0,0 +1,119 @@
+<?php
+@session_start(); //El @ evita que esta linea pinte los errores por pantalla. En este caso si la session se ha iniciado en otro archivo.
+require 'utils.php';
+require 'config/conn.php';
+require 'bootstrap.php';
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use model\Repository\UserRepository;
+
+$UserRepository = $em-> getRepository("\\Model\\Entity\\bentatecnologies_usuarios");
+
+
+
+if (!empty($_SESSION['username'])) {
+    //getDatosUsuario($_SESSION['username']);
+    $datosUser = $UserRepository->getDatosUsuario($_SESSION['username']);
+
+    //$arrayUsers = getDatosUsuario($_SESSION['username']);
+    $arrayUsers = $datosUser;
+}elseif(!isset($evitaRedireccion) || $evitaRedireccion == false) {
+        header('Location: login.php');
+} ?>
+
+<?php if (!empty($_SESSION['message'])) : ?>
+      <h2 style="text-align:center;color:green;"> <?= $_SESSION['message'] ?></h2>
+    <?php endif; unset($_SESSION['message'])
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>https://www.BentaTecnologies.com</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="contenidoPagina">
+    <header>
+        <nav class="barra">
+            <div class="principal"><a href="index.php"><img src="assets/LogoPagina.PNG" width="75" height="90"></a><img src="assets/BentaTecnologies.gif" width="375" height="90"></div>
+            <div class="divisores">
+                <a class="botones" href="login.php"><img src="assets/user.PNG" width="30" height="30">
+                    <FONT COLOR="white" SIZE=4>Login</FONT>
+                </a>
+                <a class="botones" href="registro.php"><img src="assets/user.PNG" width="30" height="30">
+                    <FONT COLOR="white" SIZE=4>Registro</FONT>
+                </a>
+                <?php if (!empty($_SESSION['username'])) {
+                    echo "<a class='botones' href='usuario.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Mi Perfil</FONT></a>";
+                } ?>
+            
+                <?php if (isset($arrayUsers)) {
+                if (isset($arrayUsers)) {
+                    /*if ($arrayUsers-['l_activo']== 1) {
+                        if ($arrayUsers['tipo'] == "Administrador" || $arrayUsers['tipo'] == "Gestor") { */
+                    if ($arrayUsers->getI_activo() == 1) {
+                        if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1) {
+                          echo "<a class='botones' href='informesAdministradorUsuario.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Informes Usuarios</FONT></a>";
+                         echo "<a class='botones' href='informesAdministradorProyectos.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Informes Proyectos</FONT></a>";
+                        }
+                         if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 ||  $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 3) {
+                          echo "<a class='botones' href='backofficeProyectos.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Buscador Proyectos</FONT></a>";
+                        }
+                        if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 ||  $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 2) {
+                          echo "<a class='botones' href='backofficeProyectosGestor.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Buscador Proyectos</FONT></a>";
+                        }
+                        if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 || $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 2 ) {
+                          echo "<a class='botones' href='misProyectos.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Mis Proyectos</FONT></a>";
+                        }
+                        if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 || $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 3) {
+                          echo "<a class='botones' href='solicitudesParticipante.php'><img src='assets/notificacion.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Solicitudes Participante</FONT></a>";
+                        }
+                       
+                        //Editar para que solo vea
+                        if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 || $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 2 ) {
+                            echo "<div class='dropdown'>
+                            <button class='dropbtn'><img class='botonFiltro' src='assets/filtrado.png'><FONT COLOR='white'>Utilidades</FONT></button>
+                            <div class='dropdown-content'>
+                                <a href='FormularioCreaEdiProyectos.php'><img class='filtrado' src='assets/editar.png'><span class='tabulacion'>Formularios de proyectos</span></a>
+                                <a href='FormularioCreaEdiTareas.php'><img class='filtrado' src='assets/editar.png'><span class='tabulacion'>Formularios Tareas</span></a>
+                                <a href='FormularioConvReunion.php'><img class='filtrado' src='assets/editar.png'><span class='tabulacion'>Convocar Reunion</span></a>
+                            </div>
+                        </div>";
+                        }
+                        //Editar para que solo vea gestor
+                         if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 || $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 2 ) {
+                            echo "<a class='botones' href='backofficegestor.php'><img src='assets/user.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Back Office Gestor</FONT></a>";
+                        }
+                         if ($arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 1 || $arrayUsers->getn_idtipo_usuario()->getid_tipo_usuario() == 2) {
+                          echo "<a class='botones' href='solicitudesGestor.php'><img src='assets/notificacion.PNG' width='30' height='30'>
+                    <FONT COLOR='white' SIZE=4>Solicitudes Gestor</FONT></a>";
+                        }
+                    }
+                } else {
+                    echo "<h2>Usted ha sido desahibilitado por el Administrador. Por favor, contacte con el administrador para que pueda volver a disfrutar de nuestra aplicaciones.</h2>";
+                }
+                } ?>
+                
+            </div>
+        </nav>
+    </header>
+    <hr>
+    <a class="usuario" href="usuario.php"><?php if (!empty($_SESSION['username'])) : ?>
+            <h2> USUARIO: <?= $_SESSION['username']
+                            ?></h2><a class="usuario" href="logout.php">SALIR</a>
+        <?php endif; ?>
+    </a>
